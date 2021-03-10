a7ddc7076c0d0df9cc918fe92002ae081fed9eeb
qaiserlab <f.anaturdasa@gmail.com>
2021-03-10 08:25:21 +0700

    fix: class component implementations

--- DataMenu.tsx
+++ DataMenu.tsx
@@ -19,57 +19,68 @@ interface PropsInterface {
   onSelect?: (event: any) => void;
 };
 
-export default function DataMenu(props: PropsInterface) {
-  const dataSource = Array.isArray(props.dataSource)?props.dataSource:[];
-  // horizontal, vertical, inline
-  const mode = (props.mode)?props.mode:'horizontal';
-  // light, dark
-  const theme = (props.theme)?props.theme:'light';
-
-  const handleSelect = ({ item, key, keyPath, selectedKeys, domEvent }) => {
-    if (props.onSelect) {
-      props.onSelect({ item, key, keyPath, selectedKeys, domEvent });
-    }
+export default class Welcome extends React.Component<PropsInterface, {}> {
+  public static defaultProps: Partial<PropsInterface> = {
+    dataSource: [],
+    // horizontal, vertical, inline
+    mode: 'horizontal',
+    // light, dark
+    theme: 'light',
   };
 
-  return (
-    <React.Fragment>
-      <Menu 
-        mode={mode} 
-        theme={theme} 
-        onSelect={handleSelect}
-      >
-        { dataSource.map((item, itemIndex) => {
-          if (!item.hidden) {
-            return (
-              <SubMenu 
-                key={(item.key)?item.key:itemIndex} 
-                title={item.title}
-                icon={item.icon}
-                disabled={item.disabled}
-              >
-                { item.childern?.map((subItem, subItemIndex) => {
-                  if (!subItem.hidden) {
-                    return (
-                      <React.Fragment>
-                        <Menu.Item 
-                          key={
-                            (subItem.key)?subItem.key:(itemIndex + '.' + subItemIndex)
-                          }
-                          icon={subItem.icon}
-                          disabled={subItem.disabled}
-                        >
-                          {subItem.title}
-                        </Menu.Item>
-                      </React.Fragment>
-                    );
-                  }
-                })}
-              </SubMenu>
-            );
-          }
-        })}
-      </Menu>
-    </React.Fragment>
-  );
+  constructor(props: PropsInterface) {
+    super(props);
+
+    // this.state = {test: true};
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect({ item, key, keyPath, selectedKeys, domEvent }) {
+    if (this.props.onSelect) {
+      this.props.onSelect({ item, key, keyPath, selectedKeys, domEvent });
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Menu 
+          mode={this.props.mode} 
+          theme={this.props.theme} 
+          onSelect={this.handleSelect}
+        >
+          { this.props.dataSource.map((item, itemIndex) => {
+            if (!item.hidden) {
+              return (
+                <SubMenu 
+                  key={(item.key)?item.key:itemIndex} 
+                  title={item.title}
+                  icon={item.icon}
+                  disabled={item.disabled}
+                >
+                  { item.childern?.map((subItem, subItemIndex) => {
+                    if (!subItem.hidden) {
+                      return (
+                        <React.Fragment>
+                          <Menu.Item 
+                            key={
+                              (subItem.key)?subItem.key:(itemIndex + '.' + subItemIndex)
+                            }
+                            icon={subItem.icon}
+                            disabled={subItem.disabled}
+                          >
+                            {subItem.title}
+                          </Menu.Item>
+                        </React.Fragment>
+                      );
+                    }
+                  })}
+                </SubMenu>
+              );
+            }
+          })}
+        </Menu>
+      </React.Fragment>
+    );
+  }
 }
